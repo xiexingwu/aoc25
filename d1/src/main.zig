2e7d9dd6e0579b0c3b289a28621b901c3883529b
Michael Xie <xie.michael.x@gmail.com>
2025-12-19 02:37:57 +0800

    d1.2

--- d1/src/main.zig
+++ d1/src/main.zig
@@ -18,31 +18,56 @@ pub fn main() !void {
     // defer allocator.deinit(stdout_buf);
 
     // const  = try std.fmt.bufPrintZ(&io_buf, "{}\n", .{pass});
-    const pass = try decodeInput("input.txt");
+    const pass = try decodeInput("input.txt", null);
     _ = try std.fs.File.stdout().write(try std.fmt.bufPrintZ(&io_buf, "{}\n", .{pass}));
+
+    const pass2 = try decodeInput("input.txt", 0x434C49434B);
+    _ = try std.fs.File.stdout().write(try std.fmt.bufPrintZ(&io_buf, "{}\n", .{pass2}));
 }
 
-fn decodeInput(input: []const u8) !usize {
+fn decodeInput(input: []const u8, method: ?usize) !usize {
     const file = try std.fs.cwd().openFile(input, .{ .mode = .read_only });
     defer file.close();
     var reader: std.fs.File.Reader = .init(file, &io_buf);
 
     var pos: isize = 50;
-    var pass: usize = 0;
+    var stops: usize = 0;
     while (try reader.interface.takeDelimiter('\n')) |line| {
         const dir = line[0];
         const delta: isize = @intCast(try std.fmt.parseInt(usize, line[1..], 10));
-        switch (dir) {
-            'L' => pos = @mod(pos - delta, 100),
-            'R' => pos = @mod(pos + delta, 100),
-            else => unreachable,
+        if (method == null) {
+            switch (dir) {
+                'L' => pos = @mod(pos - delta, 100),
+                'R' => pos = @mod(pos + delta, 100),
+                else => unreachable,
+            }
+            if (pos == 0) stops += 1;
+        } else if (method == 0x434C49434B) {
+            std.debug.print("{:3} {c}{:3} => ", .{ pos, dir, @abs(delta) });
+            switch (dir) {
+                'L' => pos -= delta,
+                'R' => pos += delta,
+                else => unreachable,
+            }
+            var passes = @abs(@divTrunc(pos, 100)); // full turns
+            passes += if (pos < 0 and pos + delta > 0) 1 else 0; // ending negative only counts as a cross if start was positive (not zero)
+            passes += if (pos == 0) 1 else 0;
+            stops += @abs(passes);
+            std.debug.print("{:4} ({:2}) => ", .{ pos, passes });
+
+            // Reset position to positive
+            pos = @mod(pos, 100);
+            std.debug.print("{:3}\n", .{pos});
         }
-        if (pos == 0) pass += 1;
     }
 
-    return pass;
+    return stops;
 }
 
-test "sample" {
-    try std.testing.expectEqual(3, try decodeInput("sample.txt"));
+test "sample part 1" {
+    try std.testing.expectEqual(3, try decodeInput("sample.txt", null));
+}
+
+test "sample part 2" {
+    try std.testing.expectEqual(6, try decodeInput("sample.txt", 0x434C49434B));
 }
